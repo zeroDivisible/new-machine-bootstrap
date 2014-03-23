cfbf0f66239722542c72b6d215b53cca0a4bf433
zeroDivisible <zerodivisible@gmail.com>
2014-03-23 23:26:51 +0000

    made it possible to bootstrap tools installed using brew

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,41 @@
-from fabric.api import run
+from fabric.api import task, run, execute
 
 
-def host_type():
-    run('uname -s')
+BREW = '/usr/local/bin/brew '
+BREW_CONFIG_DIR = 'brew/'
+
+@task
+def install_homebrew():
+    run('ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"')
+    run(BREW + ' install phinze/cask/brew-cask')
+
+
+@task
+def update_brew():
+    run(BREW + 'update')
+    run(BREW + 'upgrade')
+
+@task
+def install_from_brew():
+    execute(tab_brews)
+    execute(install_brew_apps)
+
+@task
+def tab_brews():
+    with open(BREW_CONFIG_DIR + 'taps.txt', 'r') as f:
+        taps = f.read().splitlines()
+        for tap in taps:
+            run(BREW + 'tap ' + tap)
+
+@task
+def install_brew_apps():
+    with open(BREW_CONFIG_DIR + 'tools.txt', 'r') as f:
+        tools = f.read().splitlines()
+        for tool in tools:
+            run(BREW + 'install ' + tap)
+
+
+def bootstrap():
+    execute(install_homebrew)
+    execute(update_brew)
+    execute(install_brew_apps)
